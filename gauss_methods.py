1de1bfe151bbf18cf94c0e3003941b260d0ed6b3
Mohamed Nouh <84164418+nouhh12@users.noreply.github.com>
2021-06-17 19:15:34 +0200

    Update Methods To Read File

--- gauss_methods.py
+++ gauss_methods.py
@@ -1,18 +1,19 @@
 import numpy as np
-def gauss_seidel_calc(self,entries,epsilon):
+def gauss_seidel_calc(self,entries,epsilon,x1,x2,x3):
         a=np.zeros((3,3+1))
         k=0
         iterations='i \t x1 \t\t x2 \t\t x3 \t\n'
         for i in range(3):
             for j in range(3+1):
                 try:
-                    a[i][j]=float(entries[k].get())
+                    a[i][j]=float(entries[k])
                     k+=1
                 except:
                     self.popupmsg("Please Insert Numerical Values In All Spaces!")
         x0,y0,z0=0,0,0
+        iterations += '%d \t [%.6f \t %.6f\t %.6f]\n' % (0,x1,x2,x3)
         x1,x2,x3=0,0,0
-        for i in range(50):
+        for i in range(1,51):
             x1=(a[0][3]-a[0][1]*x2-a[0][2]*x3)/a[0][0]
             x2=(a[1][3]-a[1][0]*x1-a[1][2]*x3)/a[1][1]
             x3=(a[2][3]-a[2][0]*x1-a[2][1]*x2)/a[2][2]
@@ -20,7 +21,7 @@ def gauss_seidel_calc(self,entries,epsilon):
             e2=abs((x2-y0)/x2)
             e3=abs((x3-z0)/x3)
             x0,y0,z0=x1,x2,x3
-            if e1<epsilon or e2<epsilon or e3<epsilon:
+            if e1<epsilon and e2<epsilon and e3<epsilon:
                 break
             iterations += '%d \t [%.6f \t %.6f\t %.6f]\n' % (i,x1,x2,x3)
         return iterations
@@ -30,7 +31,7 @@ def gauss_elimination_calc(self,entries,n):
         for i in range(n):
             for j in range(n+1):
                 try:
-                    a[i][j]=float(entries[k].get())
+                    a[i][j]=float(entries[k])
                     k+=1
                 except:
                     self.popupmsg("Please Insert Numerical Values In All Spaces!")
